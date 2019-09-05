c3f89aa0d0e7e9933c2b1fbffd2788b63e2d213a
System Administrator <admin@example.com>
2019-09-05 21:40:51 +0000

    Changes by 'System Administrator' on 'Demo App – 1.0.0' -- Build #1

---
+++ sn/Demo App/Dictionary Entry/x_400965_demo_app_table/active_true/number/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo App
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-09-05 17:55:33
+ * Created By  : admin
+ * Updated On  : 2019-09-05 17:55:33
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=f46c6740dbf333006a6ad001cf96198e
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
